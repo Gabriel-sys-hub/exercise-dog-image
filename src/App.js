1cc9960f11d132a5efe4772b7956b7fbe6842477
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-06-07 09:19:39 -0300

    Questão 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogPictures: '',
+      loading: true,
+    };
+    this.fetchApi = this.fetchApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  shouldComponentUpdate() {
+    const localDogImg = localStorage.getItem('dogImageUrl');
+
+    const includesTerrier = localDogImg.includes('terrier');
+
+    return !includesTerrier;
+  }
+
+  fetchApi() {
+    this.setState({ loading: true },
+      () => fetch('https://dog.ceo/api/breeds/image/random')
+        .then((result) => result.json())
+        .then((json) => this.setState((lastState) => ({
+          dogPictures: [...lastState.dogPictures, json.message],
+          loading: false,
+        }))));
+    const { dogPictures, loading } = this.state;
+    localStorage.setItem('dogImageUrl', dogPictures[dogPictures.length - 1]);
+    const localDogImg = localStorage.getItem('dogImageUrl');
+    const breed = localDogImg.replace(/[\d\w/.:]*breeds\//g, '').replace(/\/.*/g, '');
+    if (!loading) {
+      alert(breed);
+    }
+  }
+
+  render() {
+    const { dogPictures, loading } = this.state;
+    return (
+      <div className="App">
+        {loading ? (
+          <h1>loading...</h1>
+        ) : (
+          <div>
+            <img src={ dogPictures[dogPictures.length - 1] } alt="dogimage" />
+            <button type="button" onClick={ this.fetchApi }>New Dog Picture</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
